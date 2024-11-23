dd7b6755bf425ea8fe144f50a42a025e77f12bae
z <b>
2024-11-24 01:12:37 +0900

    change

--- scripts/gitconfig.local.sh
+++ scripts/gitconfig.local.sh
@@ -1,12 +1,19 @@
 #!/bin/zsh
 
+if [ $# -ne 2 ] || [ -z $1 ] || [ -z $2 ]; then
+  echo "ERROR: Invalid arguments."
+  exit 1
+fi
+
 GIT_CONFIG_LOCAL=~/.gitconfig.local
 if [ ! -e $GIT_CONFIG_LOCAL ]; then
-  echo -n ".gitconfig user.name?> "
-  read GIT_AUTHOR_NAME
-
-  echo -n ".gitconfig user.email?> "
-  read GIT_AUTHOR_EMAIL
+  # echo -n ".gitconfig user.name?> "
+  # read GIT_AUTHOR_NAME
+  #
+  # echo -n ".gitconfig user.email?> "
+  # read GIT_AUTHOR_EMAIL
+  GIT_AUTHOR_NAME=$1
+  GIT_AUTHOR_EMAIL=$2
 
   cat << EOF > $GIT_CONFIG_LOCAL
 [user]
