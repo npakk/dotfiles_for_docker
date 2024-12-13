a16db0d482f0d41414ff738a93a35f85c00a6aa0
npakk <npakk669@gmail.com>
2024-12-13 21:23:01 +0900

    chang

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,7 +5,14 @@ vim.loader.enable()
 vim.g.loaded_python_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
-vim.g.loaded_node_provider = 0
+
+-- Node.js
+if vim.fn.executable("/usr/local/bin/neovim-node-host") == 1 then
+  vim.g.node_host_prog = "/usr/local/bin/neovim-node-host"
+else
+  vim.g.loaded_node_provider = 0
+end
+
 vim.g.loaded_perl_provider = 0
 
 --[[ Commands ]]
